9601d01b39a7a954852d63bc1c5e8192f19864e0
JohnKarima <karimajohn24@gmail.com>
2022-02-09 18:30:35 +0200

    Add initial app pages and routes

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,6 @@
+function SignIn() {
+    return <div><h1>Sign In</h1></div>;
+  }
+  
+  export default SignIn;
+  
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,6 @@
+function SignUp() {
+    return <div><h1>Sign Up</h1></div>;
+  }
+  
+  export default SignUp;
+  
