06453b061c10affe7df7b6a046ae75194ddf1e52
JohnKarima <karimajohn24@gmail.com>
2022-02-09 17:56:16 +0200

    Initialize Firebase Authentication, set up database and add dummy data

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB_JJ8jBQnQgxyWd6xxtlEoR6yEVsjzQYo',
+  authDomain: 'house-marketplace-app-a8e48.firebaseapp.com',
+  projectId: 'house-marketplace-app-a8e48',
+  storageBucket: 'house-marketplace-app-a8e48.appspot.com',
+  messagingSenderId: '423847410738',
+  appId: '1:423847410738:web:78e0b04a21c5f3e048502b',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore
