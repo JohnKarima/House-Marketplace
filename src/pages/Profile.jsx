08295656a154ef01af909478e2d1499a382c6229
JohnKarima <karimajohn24@gmail.com>
2022-02-15 22:44:48 +0200

    Partially implement Display and Update user details functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { updateDoc } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { useNavigate, Link } from 'react-router-dom'
 
 function Profile() {
   const auth = getAuth()
-
+  const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -19,6 +21,10 @@ function Profile() {
     navigate('/')
   }
 
+  const onSubmit = () => {
+      console.log(123)
+  }
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -27,6 +33,20 @@ function Profile() {
           Log Out
         </button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails((prevState) => !prevState)
+            }}
+          >
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+      </main>
     </div>
   )
 }
